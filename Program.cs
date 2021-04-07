a4fd6ba88c000290b89ceef97982dbdfe5501210
eXCoreX <excoreky@gmail.com>
2021-04-07 10:17:48 +0300

    Project init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MMO_Stuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
